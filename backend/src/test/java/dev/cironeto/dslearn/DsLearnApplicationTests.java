493b10818dd2f422001b6af71b11be72599f9cd2
Ciro Neto <ciro.neto16@gmail.com>
2021-12-20 15:33:30 -0300

    initial commit - set User - Role entities and sql seed

---
+++ backend/src/test/java/dev/cironeto/dslearn/DsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cironeto.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
